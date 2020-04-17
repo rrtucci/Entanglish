fixed numeric problem with squashed entanglement calculation

entanglish/DenMat.py
@@ -413,7 +413,7 @@ class DenMat:
         comp_axes_set = set([ax for ax in all_axes if ax not in axes_set])
         return comp_axes_set
 
-    def get_entropy(self, method='eigen'):
+    def get_entropy(self, approx='eigen'):
         """
         This method returns an exact entropy of density matrix self. Uses
         natural log for entropy. Assumes eigenvalues of self are
@@ -421,8 +421,8 @@ class DenMat:
 
         Parameters
         ----------
-        method : str        
-            method used to calculate log of array. Either 'eigen' or 'pade' 
+        approx : str
+            approx used to calculate log of array. Either 'eigen' or 'pade'
 
         Returns
         -------
@@ -430,18 +430,18 @@ class DenMat:
 
         """
         ent = 0.0
-        if method == 'eigen':
+        if approx == 'eigen':
             evas = np.real(np.linalg.eigvalsh(self.arr))
             ent = ut.get_entropy_from_probs(evas)
-        elif method == 'pade':
+        elif approx == 'pade':
             ent = - np.trace(np.dot(self.arr, la.logm(self.arr))).real
         else:
-            assert False, 'unsupported method for ' +\
+            assert False, 'unsupported approx for ' +\
                           'calculating entropy of a density matrix.'
 
         return ent
 
-    def get_mutual_info(self, traced_axes_set, method='eigen'):
+    def get_mutual_info(self, traced_axes_set, approx='eigen'):
         """
         This method returns the mutual information for x_axes = list(
         traced_axes_set) and y_axes = row axes not in x_axes. Uses natural
@@ -451,8 +451,8 @@ class DenMat:
         Parameters
         ----------
         traced_axes_set : set[int]
-        method : str        
-            method used to calculate log of array. Either 'eigen' or 'pade' 
+        approx : str
+            approx used to calculate log of array. Either 'eigen' or 'pade'
 
         Returns
         -------
@@ -466,9 +466,9 @@ class DenMat:
 
         dm_x = self.get_partial_tr(set(y_axes))
         dm_y = self.get_partial_tr(set(x_axes))
-        mi = - self.get_entropy(method) \
-             + dm_x.get_entropy(method) \
-             + dm_y.get_entropy(method)
+        mi = - self.get_entropy(approx) \
+             + dm_x.get_entropy(approx) \
+             + dm_y.get_entropy(approx)
         return mi
 
     def dm_op(self, right, arr_op):
@@ -751,14 +751,14 @@ class DenMat:
         """
         return np.linalg.norm(self.arr)
 
-    def sqrt(self, method='eigen'):
+    def sqrt(self, approx='eigen'):
         """
         This method returns a DenMat which is the matrix square root of self.
 
         Parameters
         ----------
-        method : str
-            method used to calculate sqrt. Either 'eigen' or 'pade'.
+        approx : str
+            approx used to calculate sqrt. Either 'eigen' or 'pade'.
 
         Returns
         -------
@@ -766,23 +766,23 @@ class DenMat:
 
         """
         sqrtm_arr = None
-        if method == 'eigen':
+        if approx == 'eigen':
             sqrtm_arr = ut.fun_of_herm_arr(lambda x: np.sqrt(x) if x > 0 else
             0, self.arr)
-        elif method == 'pade':
+        elif approx == 'pade':
             sqrtm_arr = la.sqrtm(self.arr)
         else:
-            assert False, 'unsupported method'
+            assert False, 'unsupported approx'
         return DenMat(self.num_rows, self.row_shape, sqrtm_arr)
 
-    def exp(self, method='eigen'):
+    def exp(self, approx='eigen'):
         """
         This method returns a DenMat which is the matrix exponential of self.
 
         Parameters
         ----------
-        method : str
-            method used to calculate exp. Either 'eigen' or 'pade'.
+        approx : str
+            approx used to calculate exp. Either 'eigen' or 'pade'.
 
         Returns
         -------
@@ -790,29 +790,22 @@ class DenMat:
 
         """
         expm_arr = None
-        if method == 'eigen':
+        if approx == 'eigen':
             expm_arr = ut.fun_of_herm_arr(np.exp, self.arr)
-        elif method == 'pade':
+        elif approx == 'pade':
             expm_arr = la.expm(self.arr)
         else:
-            assert False, 'unsupported method'
+            assert False, 'unsupported approx'
         return DenMat(self.num_rows, self.row_shape, expm_arr)
 
-    def log(self, method='eigen', clipped=True, eps=1e-4,
-            clip_to_zero=False):
+    def log(self, approx='eigen'):
         """
         This method returns a DenMat which is the matrix natural log of self.
 
         Parameters
         ----------
-        method : str
-            method used to calculate the natural log. Either 'eigen' or 'pade'.
-        clipped : bool
-            clips logs (see ut.clipped_log_of_vec) iff this is True
-        eps : float
-            used only if clipping log
-        clip_to_zero : bool
-            used only if clipping log
+        approx : str
+            approx used to calculate the natural log. Either 'eigen' or 'pade'.
 
         Returns
         -------
@@ -822,20 +815,12 @@ class DenMat:
         # self.add_const_to_diag_of_arr(1e-8)
         # self.normalize_diag_of_arr()
         logm_arr = None
-        if method == 'eigen':
-            if clipped:
-                logm_arr = ut.fun_of_herm_arr(ut.clipped_log_of_vec,
-                                              self.arr,
-                                              eps=eps,
-                                              clip_to_zero=clip_to_zero)
-            else:
-                logm_arr = ut.fun_of_herm_arr(np.log,
-                                              self.arr)
-
-        elif method == 'pade':
+        if approx == 'eigen':
+            logm_arr = ut.fun_of_herm_arr(ut.clipped_log_of_vec, self.arr)
+        elif approx == 'pade':
             logm_arr = la.logm(self.arr)
         else:
-            assert False, 'unsupported method'
+            assert False, 'unsupported approx'
         return DenMat(self.num_rows, self.row_shape, logm_arr)
 
     def positive_part(self, threshold=1e-5):

entanglish/EntangCase.py
@@ -16,13 +16,13 @@ class EntangCase:
 
     Attributes
     ----------
-    method : str
-        method used to calculate the (natural) log of a density matrix that
+    approx : str
+        approx used to calculate the (natural) log of a density matrix that
         appears in the definition of the von Neumann entropy. This parameter
         can be either 'eigen', 'pade', 'pert'.
     num_bstrap_steps : int
         number bootstrap steps used in perturbation theory. Only used if
-        method = 'pert'
+        approx = 'pert'
     num_row_axes : int
         number of row axes, same as number of qudits, equal to len(
         row_shape) of a DenMat
@@ -30,7 +30,7 @@ class EntangCase:
 
     """
 
-    def __init__(self, num_row_axes, method='eigen', num_bstrap_steps=1,
+    def __init__(self, num_row_axes, approx='eigen', num_bstrap_steps=1,
                  verbose=False):
         """
         Constructor
@@ -38,7 +38,7 @@ class EntangCase:
         Parameters
         ----------
         num_row_axes : int
-        method : str
+        approx : str
         num_bstrap_steps : int
         verbose : bool
 
@@ -48,8 +48,8 @@ class EntangCase:
 
         """
         self.num_row_axes = num_row_axes
-        self.method = method
-        assert method in ['eigen', 'pade', 'pert']
+        self.approx = approx
+        assert approx in ['eigen', 'pade', 'pert']
         self.num_bstrap_steps = num_bstrap_steps
         assert num_bstrap_steps > 0
         self.verbose = verbose
@@ -102,7 +102,7 @@ class EntangCase:
         Parameters
         ----------
         axes_subset : set[int]
-            the entanglement is calculate for parts axes_subset and its
+            the entanglement is calculated for parts axes_subset and its
             complement
 
         Returns
@@ -171,11 +171,12 @@ class EntangCase:
         """
         This method constructs a dictionary that we call an entanglement
         profile. Given a state with num_row_axes qudits, this method
-        calculates a (bipartite) entanglement for each possible bi-partition of
-        range( num_row_axes). By a bi-partition we mean two nonempty disjoint
-        subsets whose union is range(num_row_axes). An entanglement profile
-        is a dictionary mapping bi-partition half-size to a dictionary that
-        maps each bi-partition of that half-size to its entanglement.
+        calculates a (bipartite) entanglement for each possible bi-partition
+        of range( num_row_axes). By a bi-partition we mean two nonempty
+        disjoint subsets whose union is range(num_row_axes). An entanglement
+        profile is a dictionary mapping bi-partition half-size to a
+        dictionary that maps each bi-partition of that half-size to its
+        entanglement.
 
         Returns
         -------
@@ -236,34 +237,33 @@ class EntangCase:
                         if row_shape:
                             line += ', max-entang=' + \
                                 "{:8.5f}".format(EntangCase.get_max_entag(
-                                row_shape, comb, comb_c)) + '\n'
+                                    row_shape, comb, comb_c)) + '\n'
                         else:
                             line += '\n'
                 all_lines += line
         print(all_lines)
 
-    def sqrt(self, dm, method=None):
+    def sqrt(self, dm, approx=None):
         """
         This method is a simple wrapper for dm.sqrt() so that all usages
-        inside the class utilize the same method self.method which is an
-        attribute of the class.
+        inside the class utilize approx=self.approx if approx!=None.
 
         Parameters
         ----------
         dm : DenMat
-        method : str
+        approx : str
 
         Returns
         -------
         DenMat
 
         """
-        if method is None:
-            method = self.method
+        if approx is None:
+            approx = self.approx
         dm_out = None
-        if method in ['eigen', 'pade']:
-            dm_out = dm.sqrt(method=method)
-        elif method == 'pert':
+        if approx in ['eigen', 'pade']:
+            dm_out = dm.sqrt(approx=approx)
+        elif approx == 'pert':
             esys = DenMatPertTheory.do_bstrap_with_separable_dm0(
                 dm, self.num_bstrap_steps)
             dm_out = DenMat.get_fun_of_dm_from_eigen_sys(
@@ -272,28 +272,27 @@ class EntangCase:
             assert False
         return dm_out
 
-    def exp(self, dm, method=None):
+    def exp(self, dm, approx=None):
         """
         This method is a simple wrapper for dm.exp() so that all usages
-        inside the class utilize the same method self.method which is an
-        attribute of the class.
+        inside the class utilize approx=self.approx if approx!=None.
 
         Parameters
         ----------
         dm : DenMat
-        method : str
+        approx : str
 
         Returns
         -------
         DenMat
 
         """
-        if method is None:
-            method = self.method
+        if approx is None:
+            approx = self.approx
         dm_out = None
-        if method in ['eigen', 'pade']:
-            dm_out = dm.exp(method)
-        elif method == 'pert':
+        if approx in ['eigen', 'pade']:
+            dm_out = dm.exp(approx)
+        elif approx == 'pert':
             esys = DenMatPertTheory.do_bstrap_with_separable_dm0(
                 dm, self.num_bstrap_steps)
             dm_out = DenMat.get_fun_of_dm_from_eigen_sys(
@@ -302,43 +301,31 @@ class EntangCase:
             assert False
         return dm_out
 
-    def log(self, dm, method=None, clipped=True, eps=1e-4,
-            clip_to_zero=False):
+    def log(self, dm, approx=None):
         """
         This method is a simple wrapper for dm.log() so that all usages
-        inside the class utilize the same method self.method which an
-        attribute of the class.
+        inside the class utilize approx=self.approx if approx!=None.
 
         Parameters
         ----------
         dm : DenMat
-        method : str
-        clipped : bool
-        eps : float
-        clip_to_zero : bool
+        approx : str|None
 
         Returns
         -------
         DenMat
 
         """
-        if method is None:
-            method = self.method
+        if approx is None:
+            approx = self.approx
         dm_out = None
-        if method in ['eigen', 'pade']:
-            dm_out = dm.log(method=method,
-                            clipped=clipped,
-                            eps=eps,
-                            clip_to_zero=clip_to_zero)
-        elif method == 'pert':
+        if approx in ['eigen', 'pade']:
+            dm_out = dm.log(approx=approx)
+        elif approx == 'pert':
             esys = DenMatPertTheory.do_bstrap_with_separable_dm0(
                 dm, self.num_bstrap_steps)
-            fun = np.log
-            if clipped:
-                fun = ut.clipped_log_of_vec
             dm_out = DenMat.get_fun_of_dm_from_eigen_sys(
-                dm.num_rows, dm.row_shape, esys, fun,
-                clipped=clipped, eps=eps, clip_to_zero=clip_to_zero)
+                dm.num_rows, dm.row_shape, esys, np.log)
         else:
             assert False
         return dm_out

entanglish/SquashedEnt.py
@@ -8,9 +8,9 @@ class SquashedEnt(EntangCase):
     """
     This class is a child of class EntangCase. Its purpose is to calculate
     the (bipartite) quantum entanglement Exy of a mixed state density matrix
-    Dxy with parts x and y. Exy is defined here as the squashed entanglement
-    ( see Wikipedia Ref. 1 for more detailed description and original
-    references of squashed entanglement)
+    Dxy with parts x and y. Exy is defined here as the squashed
+    entanglement. (See Wikipedia Ref. 1 for more detailed description and
+    original references of squashed entanglement.)
 
     Exy = (1/2)*min S(x : y | h)
 
@@ -41,17 +41,11 @@ class SquashedEnt(EntangCase):
     Dxy : DenMat          
         The input density matrix den_mat, after permuting its axes so that 
         the axes are given in the precise order self.x_axes + self.y_axes.
-    Dxy_proj_ops : (DenMat, DenMat)
-        projection operators for zero and nonzero eigenvalues of Dxy
-    Dxy_sqrt : DenMat
-        square root of Dxy
     den_mat : DenMat
     calc_formation_ent : bool
-        True iff want special case of entanglement of formation
+        True iff want to calculate entanglement of formation
     eps_w : float
         weights (probabilities) < eps_w are rounded to zero
-    eps_log : float
-        logs are clipped at this value. log(x) = log(eps_log) if x < eps_log
     num_ab_steps : int
         number of iterations of Arimoto Blahut algo
     num_hidden_states : int
@@ -68,7 +62,7 @@ class SquashedEnt(EntangCase):
 
     """
     def __init__(self, den_mat, num_ab_steps, num_hidden_states=0,
-                method="eigen", recursion_init='eigen',
+                approx="eigen", recursion_init='eigen',
                 verbose=False):
         """
         Constructor
@@ -79,7 +73,7 @@ class SquashedEnt(EntangCase):
         num_hidden_states : int
             number of states of h in the Dxyh defined in class doc string
         num_ab_steps : int
-        method : str
+        approx : str
         recursion_init : str
         verbose : bool
 
@@ -87,18 +81,16 @@ class SquashedEnt(EntangCase):
         -------
 
         """
-        assert method != "pert", "evaluation of squashed ent not implemented' \
-                                 'for method=='pert'"
-        EntangCase.__init__(self, len(den_mat.row_shape), method=method,
+        assert approx != "pert", "evaluation of squashed ent not implemented' \
+                                 'for approx=='pert'"
+        EntangCase.__init__(self, len(den_mat.row_shape), approx=approx,
                             verbose=verbose)
         self.den_mat = den_mat
         self.num_ab_steps = num_ab_steps
         self.num_hidden_states = num_hidden_states
         self.recursion_init = recursion_init
 
-        self.eps_w = 1e-5
-        # results very sensitive to eps_log, but not to eps_w
-        self.eps_log = 2e-4
+        self.eps_w = 1e-6
 
         self.x_axes = None
         self.y_axes = None
@@ -162,8 +154,6 @@ class SquashedEnt(EntangCase):
         self.y_axes = [k for k in range(num_row_axes) if k not in self.x_axes]
         num_y_axes = len(self.y_axes)
         self.Dxy = self.den_mat.get_rho_xy(self.x_axes, self.y_axes)
-        # self.Dxy_proj_ops = self.Dxy.get_eigenvalue_proj_ops()
-        self.Dxy_sqrt = self.sqrt(self.Dxy)
 
         Kxy_a = []
         if self.recursion_init == 'stat-pt':
@@ -268,8 +258,9 @@ class SquashedEnt(EntangCase):
         for step in range(self.num_ab_steps):
             Kxy_a, entang, err = self.next_step(Kxy_a)
             if self.verbose:
-                print('--ab step=', step, ', entang=', entang,
-                      ", err=", err)
+                print('--ab step=', step,
+                      ', entang=', "{:.6f}".format(entang),
+                      ", err=", "{:.6f}".format(err))
         return entang
 
     def next_step(self, Kxy_a):
@@ -291,7 +282,9 @@ class SquashedEnt(EntangCase):
         Delta_xy[alp] matrices. This variance tends to zero as num_ab_steps
         tends to infinity
 
-        mean_alp x[alp] = average over alp of x[alp]
+        mean_alp x[alp] = weighted average (with weights w_a) over alp of x[
+        alp]
+
         err = mean_alp norm(Delta_xy[alp] - mean_alp Delta_xy[alp])
 
         Parameters
@@ -317,108 +310,71 @@ class SquashedEnt(EntangCase):
         entang = 0
         for alp in range(self.num_hidden_states):
             Kxy_alp = Kxy_a[alp]
-            if Kxy_alp is not None:
+            if Kxy_alp is None:
+                w_alp = 0.0
+            else:
                 w_alp = Kxy_alp.trace()
                 if w_alp < self.eps_w:
                     w_alp = 0
                     Kxy_alp = None
                 else:
                     num_nonzero_w_alp += 1
-            else:
-                w_alp = 0.0
             w_a.append(w_alp)
-            if Kxy_alp is not None:
+            if Kxy_alp is None:
+                log_Dxy_alp = None
+                log_Dx_Dy_alp = None
+            else:
                 Dxy_alp = Kxy_alp*(1/w_alp)
-                log_Dxy_alp = self.log(Dxy_alp, eps=self.eps_log)
+                log_Dxy_alp = self.log(Dxy_alp)
 
                 Dx_alp = Dxy_alp.get_partial_tr(set_y)
                 Dy_alp = Dxy_alp.get_partial_tr(set_x)
                 log_Dx_Dy_alp = self.log(DenMat.get_kron_prod_of_den_mats(
-                                [Dx_alp, Dy_alp]), eps=self.eps_log)
+                                [Dx_alp, Dy_alp]))
 
-                Delta_xy += log_Dxy_alp - log_Dx_Dy_alp
-
-                case = True
-                # case = not self.calc_formation_ent
-                if case:
-                    entang += (Dxy_alp*(log_Dxy_alp - log_Dx_Dy_alp)).\
-                        trace()*w_alp/2
-                else:  # slightly more precise
-                    ecase = PureStEnt(Dxy_alp, method=self.method)
-                    entang += ecase.get_entang(set_x)*w_alp
-
-            else:
-                log_Dxy_alp = None
-                log_Dx_Dy_alp = None
+                Delta_xy_alp = log_Dxy_alp - log_Dx_Dy_alp
+                Delta_xy += Delta_xy_alp*w_alp
+                # print('cccvvvbbb', Dxy_alp,  w_alp)
+                entang += (Dxy_alp * Delta_xy_alp).trace() * w_alp / 2
             log_Dxy_a.append(log_Dxy_alp)
             log_Dx_Dy_a.append(log_Dx_Dy_alp)
-
-        Delta_xy = Delta_xy*(1/num_nonzero_w_alp)
 
         err = 0
         for alp in range(self.num_hidden_states):
             if log_Dxy_a[alp] is not None:
-                err += (log_Dxy_a[alp] - log_Dx_Dy_a[alp] - Delta_xy).norm()
-        err /= num_nonzero_w_alp
+                Delta_xy_alp = log_Dxy_a[alp] - log_Dx_Dy_a[alp]
+                err += (Delta_xy_alp - Delta_xy).norm()*w_a[alp]
 
         print_w_a = False
         if print_w_a:
             print('w_a=', w_a, 'sum=', np.sum(np.array(w_a)))
 
         new_Kxy_a = []
-        sum_numerator = DenMat(self.Dxy.num_rows, self.Dxy.row_shape)
-        sum_numerator.set_arr_to_zero()
         for alp in range(self.num_hidden_states):
-            if log_Dx_Dy_a[alp] is not None:
-                expo_arg = Delta_xy + log_Dx_Dy_a[alp]
-                Kxy_alp = Kxy_a[alp]
-                if Kxy_alp is not None:
-                    #proj0, proj1 = Kxy_alp.get_eigenvalue_proj_ops()
-                    #proj0, proj1 = self.Dxy_proj_ops
-                    #proj0, proj1 = expo_arg.get_eigenvalue_proj_ops()
-                    #print("ggggggg", expo_arg.norm())
-                    #expo_arg = proj1*expo_arg*proj1
-                    #print("gggggggggggafter", expo_arg.norm()
-                    pass
+            if log_Dx_Dy_a[alp] is None:
+                new_Kxy_alp = None
+            else:
+                arg_of_exp = Delta_xy + log_Dx_Dy_a[alp]
+                evas, evec_cols = np.linalg.eigh(arg_of_exp.arr)
+                # set positive evas to zero
+                evas = np.array([min(x, 0) for x in evas])
                 if not self.calc_formation_ent:
-                    new_Kxy_alp = self.exp(expo_arg)*w_a[alp]
-                    #new_Kxy_alp *= (proj0*eps_in_log + proj1)
-                    #new_Kxy_alp *= eps_in_log
+                    arr = ut.fun_of_herm_arr_from_eigen_sys(
+                        np.exp, evas, evec_cols)
+                    Dxy_alp = DenMat(
+                        self.Dxy.num_rows, self.Dxy.row_shape, arr)
+                    new_Kxy_alp = Dxy_alp*w_a[alp]
                 else:
-                    evas, evec_cols = np.linalg.eigh(expo_arg.arr)
                     max_pos = np.argmax(evas)
                     vec = evec_cols[:, max_pos]
                     arr = np.outer(vec, np.conj(vec)) * np.exp(evas[max_pos])
-                    new_Kxy_alp = DenMat(self.Dxy.num_rows,
-                                         self.Dxy.row_shape, arr)
-                # _, proj1 = self.Dxy_proj_ops
-                # new_Kxy_alp = proj1*new_Kxy_alp*proj1
+                    new_Kxy_alp = DenMat(
+                        self.Dxy.num_rows, self.Dxy.row_shape, arr)
 
-                sum_numerator += new_Kxy_alp
-            else:
-                new_Kxy_alp = None
             new_Kxy_a.append(new_Kxy_alp)
 
-        inv_sum_numerator = sum_numerator.pseudo_inv(eps=self.eps_w)
-        for alp in range(self.num_hidden_states):
-            # print('rrrrr', np.linalg.norm(self.Dxy.arr),
-            #             np.linalg.norm(inv_sum_numerator.arr),
-            #             np.linalg.norm(new_Kxy_a[alp].arr))
-            if new_Kxy_a[alp] is not None:
-                root = self.Dxy_sqrt*self.sqrt(inv_sum_numerator)
-                new_Kxy_alp = root*new_Kxy_a[alp]*root.herm()
-
-                # didn't work too well:
-                # new_Kxy_alp = self.Dxy*inv_sum_numerator*new_Kxy_a[alp]
-                # new_Kxy_alp = (new_Kxy_alp + new_Kxy_alp.herm())*(1/2)
-
-                new_Kxy_a[alp] = new_Kxy_alp
         if not self.calc_formation_ent:
             new_Kxy_a = self.regulate(new_Kxy_a)
-
-        # not as precise, only valid asymptotically
-        # entang_lim = (self.Dxy*Delta_xy).trace()/2
-        # print("entang_lim", entang_lim)
 
         return new_Kxy_a, entang, err
 

entanglish/utilities.py
@@ -110,7 +110,7 @@ def clip(x, limits):
     return min(max(limits[0], x), limits[1])
 
 
-def clipped_log_of_vec(vec, eps=1e-5, clip_to_zero=False):
+def clipped_log_of_vec(vec, eps=1e-10, clip_to_zero=False):
     """
     This method takes as input a int|float or a 1D array of floats. It
     returns the log element-wise of that array, except when an element of
@@ -184,30 +184,6 @@ def positive_part_of_vec(vec):
     return np.array(li_new)
 
 
-def max_or_zero_of_vec(vec):
-    """
-    This method takes as input a int|float or a 1D array of floats. It
-    returns the array, with all items except the max item replaced by zero
-
-    Parameters
-    ----------
-    vec : int|float|np.ndarray
-
-    Returns
-    -------
-    np.ndarray
-
-    """
-    vec1 = vec
-    if isinstance(vec, (int, float)):
-        vec1 = [vec]
-    li = list(vec1)
-    (xmax, i) = max((x, i) for i, x in enumerate(li))
-    vec1 = np.zeros((len(li),))
-    vec1[i] = xmax
-    return vec1
-
-
 def fun_of_herm_arr_from_eigen_sys(fun_of_evas, evas, evec_cols,
                                    **fun_kwargs):
     """
@@ -246,6 +222,24 @@ def fun_of_herm_arr_from_eigen_sys(fun_of_evas, evas, evec_cols,
         umat[:, k] *= fun_of_evas(evas[k], **fun_kwargs)
     # print(',,', evec_cols)
     return np.dot(umat, umat_H)
+
+
+def herm_arr_from_eigen_sys(evas, evec_cols):
+    """
+    Same as fun_of_herm_arr_from_eigen_sys() but for special case when
+    fun_of_evas() is identity.
+
+    Parameters
+    ----------
+    evas : np.ndarray
+    evec_cols : np.ndarray
+
+    Returns
+    -------
+    np.ndarray
+
+    """
+    return fun_of_herm_arr_from_eigen_sys(lambda x: x, evas, evec_cols)
 
 
 def fun_of_herm_arr(fun_of_evas, herm_arr, **fun_kwargs):